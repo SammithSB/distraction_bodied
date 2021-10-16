790d4848ca482b4ef3a2850d7059603ebf50ada5
SammithSB <sammithsb@gmail.com>
2021-10-16 20:40:06 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# distraction_bodied
-chrome extension that doesn't allow to open new tabs in a unique way.
+Chrome extension that doesn't allow to open new tabs in a unique way.
+
+Whenever you try to open a new tab it opens a youtube video that is disappointed in you for getting distracted.
+We are just overriding chrome url that is all, there are 10 video links which are randomly opened evrey time you click on new tab.
+
+Improevments:
+The extension is always on, need to a toggle kind of feature to switch it on and off whenever you want to.
+Need to add more and/or better video links.
+
+
+
---
+++ code.js
@@ -0,0 +1,39 @@
+var i=Math.ceil(Math.random() * 11);
+if(i==1){
+    window.open("https://youtu.be/M_g3-Z3q9iY", "_self");
+}
+else if(i==2){
+    window.open("https://www.youtube.com/watch?v=V97F-d7-HE8", "_self");
+}
+else if(i==3){
+    window.open("https://www.youtube.com/watch?v=IScVoQiYL1c", "_self");
+}
+else if(i==4){
+    window.open("https://www.youtube.com/watch?v=RXNHpmdwvek", "_self");
+}
+else if(i==5){
+    window.open("https://www.youtube.com/watch?v=bxd6jdcmb2k", "_self");
+}
+else if(i==6){
+    window.open("https://www.youtube.com/watch?v=syPxMG-o0as", "_self");
+    
+}
+else if(i==7){
+    window.open("https://youtu.be/l4Xt8lAhRFY", "_self");
+}
+else if(i==8){
+    window.open("https://youtu.be/_OY_pNOIao4", "_self");
+}
+else if(i==9){
+    window.open("https://youtu.be/Qhe8dO_qOK0", "_self");
+}
+else if(i==10){
+    window.open("https://youtu.be/ZkHvq8yuvCU", "_self");
+}
+else if(i==11){
+    window.open("https://youtu.be/cGZaUtYojzc", "_self");
+}
+
+
+
+
